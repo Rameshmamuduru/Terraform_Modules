7d264fd27016b096fb598398101dce06dacb9d02
Ramesh Mamuduru <rameshmamuduru7799@gmail.com>
2026-06-19 09:16:10 +0530

    updated

--- security_groups/output.tf
+++ security_groups/output.tf
@@ -1,4 +1,15 @@
 output "security_group_id" {
-  
+  description = "security group ID"
   value = aws_security_group.this
+}
+
+output "security_group_id" {
+  description = "security group ARN"
+  value = aws_security_group.this.arn
+}
+
+output "security_group_name" {
+  description = "security group name"
+  value = aws_security_group.this.name
+  
 }
--- security_groups/variables.tf
+++ security_groups/variables.tf
@@ -1,41 +1,43 @@
 variable "name" {
-    type = string
-    description = "Name security group"  
+  type        = string
+  description = "Security Group Name"
 }
 
 variable "description" {
-    type = string
-    description = "Description security group"
+  type        = string
+  description = "Security Group Description"
 }
 
 variable "vpc_id" {
-    type = string
-    description = "VPC ID for security group"
-  
+  type        = string
+  description = "VPC ID"
 }
 
 variable "ingress_rules" {
-    type = list(object({
-        from_port   = number
-        to_port     = number
-        protocol    = string
-        cidr_blocks = list(string)
-        description = string
-    }))
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
 
-    default = []    
-  
+  default = []
 }
 
 variable "egress_rules" {
-    type = list(object({
-        from_port   = number
-        to_port     = number
-        protocol    = string
-        cidr_blocks = list(string)
-        description = string
-    }))
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
 
-    default = []    
-  
+  default = []
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
